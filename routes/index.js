76298549e39f2c109a11b8ffada9afe8440ae8fa
JacobDiEdwardo <jacob.diedwardo@gmail.com>
2019-03-13 15:02:58 -0400

    one more...

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 const paymentApi = require('./payment');
 
 const configureRoutes = app => {
+    app.all('/', function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        next()
+    });
     paymentApi(app);
 };
 
