065aef8edbb5fcf062eafa8166922761bf4de4ef
JacobDiEdwardo <jacob.diedwardo@gmail.com>
2019-03-13 16:08:11 -0400

    Adjusted access-control-allow-methods

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const configureServer = app => {
   app.options('*', cors())
   app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Methods', '*');
     res.setHeader('Access-Control-Allow-Headers', '*');
     next();
   });
